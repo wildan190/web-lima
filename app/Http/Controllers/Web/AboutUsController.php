89598992d7f86d600a10238cf298a75d71705d57
Wildan Belfiore <mikewildan@outlook.com>
2025-07-01 01:54:22 +0700

    Add AboutUsController and NewsRoomController with latest news retrieval; create corresponding views

---
+++ app/Http/Controllers/Web/AboutUsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Sport;
+use App\Models\WebContact;
+use App\Models\WebProfile;
+use Illuminate\Http\Request;
+
+class AboutUsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $webProfile = WebProfile::first(); // hanya ambil satu
+        $sports = Sport::all(); // ambil semua sport dan logo
+        $WebContact = WebContact::first(); // hanya ambil satu kontak
+
+        $newsLatest = \App\Models\News::orderBy('created_at', 'desc')->take(5)->get();
+
+        return view('web.about', compact('webProfile', 'sports', 'WebContact', 'newsLatest'));
+    }
+}
