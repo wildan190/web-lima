5678405d7c9dec2f8a32c4c04dea7b983b2eb75c
unknown <budidendi1234@gmail.com>
2025-07-27 10:35:15 +0700

    fix url path

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -50,32 +50,32 @@
     <meta name="application-name" content="LIMA" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="theme-color" content="#E02A26" />
-    <link rel="manifest" href="assets/js/data/manifest.json" />
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/homescreen/favicon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/homescreen/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/homescreen/favicon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="144x144" href="assets/img/homescreen/android-icon-144x144.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/homescreen/android-icon-192x192.png" />
+    <link rel="manifest" href="{{ asset('assets/js/data/manifest.json') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/homescreen/favicon-16x16.png') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/homescreen/favicon-32x32.png') }}" />
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/homescreen/favicon-96x96.png') }}" />
+    <link rel="icon" type="image/png" sizes="144x144" href="{{ asset ('assets/img/homescreen/android-icon-144x144.png') }}"/>
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/img/homescreen/android-icon-192x192.png') }}" />
 
     <!--windows microsoft-->
     <meta name="msapplication-TileColor" content="#E02A26" />
-    <meta name="msapplication-TileImage" content="assets/img/homescreen/ms-icon-144x144.png" />
+    <meta name="msapplication-TileImage" content="{{ asset('assets/img/homescreen/ms-icon-144x144.png') }}" />
 
     <!--apple add to home screen-->
     <meta name="apple-mobile-web-app-title" content="LIMA" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="#E02A26" />
-    <link rel="apple-touch-icon" href="assets/img/homescreen/apple-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/homescreen/apple-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/homescreen/apple-icon-60x60.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/homescreen/apple-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/homescreen/apple-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/homescreen/apple-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/homescreen/apple-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/homescreen/apple-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/homescreen/apple-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/homescreen/apple-icon-180x180.png" />
-    <link rel="apple-touch-startup-image" href="assets/img/homescreen/apple-icon.png" />
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/homescreen/apple-icon.png' }}" />
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/homescreen/apple-icon-57x57.png' }}" />
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/img/homescreen/apple-icon-60x60.png' }}" />
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/homescreen/apple-icon-72x72.png' }}" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/homescreen/apple-icon-76x76.png' }}" />
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/homescreen/apple-icon-114x114.png' }}" />
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/img/homescreen/apple-icon-120x120.png' }}" />
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/img/homescreen/apple-icon-144x144.png' }}" />
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/img/homescreen/apple-icon-152x152.png' }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/homescreen/apple-icon-180x180.png' }}" />
+    <link rel="apple-touch-startup-image" href="{{ asset('assets/img/homescreen/apple-icon.png' }}" />
 
     <!-- Schema.org Markup -->
     <script type="application/ld+json">
