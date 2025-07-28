ce6bde8bdd9479016a18032f9e30e38d98d843aa
Wildan Belfiore <mikewildan@outlook.com>
2025-07-28 16:11:51 +0700

    implement all page for news localize section

--- resources/views/web/gallery.blade.php
+++ resources/views/web/gallery.blade.php
@@ -641,9 +641,9 @@
     <section class="latest-news">
         <div class="container">
             <div class="news-left">
-                <h2>Latest <strong>News</strong></h2>
-                <p>Here is some breaking news especially for you.</p>
-                <a href="{{ route('news') }}" class="btn-see-more">See More</a>
+                <h2>{{ __('messages.latest_news') }}</h2>
+                <p>{{ __('messages.breaking_news') }}</p>
+                <a href="{{ route('news') }}" class="btn-see-more">{{ __('messages.see_more') }}</a>
             </div>
 
             <div class="news-right">
@@ -660,9 +660,11 @@
                                 <div class="news-img">
                                     <img src="{{ $news->picture_upload }}" alt="{{ $news->title }}">
                                     <div class="overlay">
-                                        <p>{{ $news->created_at->format('d M Y') }} &nbsp;•&nbsp; News</p>
+                                        <p>{{ $news->created_at->format('d M Y') }} &nbsp;•&nbsp;
+                                            {{ __('messages.news') }}</p>
                                         <h4>{{ \Illuminate\Support\Str::limit($news->title, 60) }}</h4>
-                                        <a href="{{ route('news.detail', $news->slug) }}"><span>Read →</span></a>
+                                        <a
+                                            href="{{ route('news.detail', $news->slug) }}"><span>{{ __('messages.read') }}</span></a>
                                     </div>
                                 </div>
                             </a>
@@ -675,7 +677,6 @@
                     <span>&#10095;</span>
                 </button>
             </div>
-
         </div>
     </section>
 @endsection
